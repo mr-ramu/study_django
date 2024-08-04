b368b0c11b5c8561fc062120f2ff35dd367b2867
mr-ramu <raimugipan8769@gmail.com>
2024-08-04 23:59:24 +0900

    p162-p250

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -1,4 +1,20 @@
 from django import forms
+from.models import Friend
 
-class HelloForm(forms.Form):
-  check = forms.BooleanField(label='CheckBox', required=False)
+class FriendForm(forms.ModelForm):
+  class Meta:
+    model = Friend
+    fields = ['name', 'mail', 'gender', 'age', 'birthday']
+
+
+
+
+class FriendForm(forms.Form):
+  name = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'forms-control'}))
+  mail = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control'}))
+  gender = forms.BooleanField(label='Gender', widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+  age = forms.IntegerField(label='Age', widget=forms.NumberInput(attrs={'class':'form-control'}))
+  birthday = forms.DateField(label='Birth', widget=forms.DateInput(attrs={'class':'form-control'}))
+
+class FindForm(forms.Form):
+  find = forms.CharField(label='Find', required=False, widget=forms.TextInput(attrs={'class':'form-control'}))
--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,24 +1,77 @@
 from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from django.shortcuts import redirect
+from .models import Friend
+from .forms import FriendForm
+from .forms import FindForm
+from django.views.generic import ListView, DetailView
 
-class HelloView(TemplateView):
-  def __init__(self):
-    self.params = {
-      'title':'Hello',
-      'form':HelloForm(),
-      'result':None,
-    }
-    
-  def get(self, request):
-    return render(request, 'hello/index.html', self.params)
+def find(request):
+  if (request.method == "POST"):
+    form = FindForm(request.POST)
+    find = request.POST['find']
+    data = Friend.objects.filter(name__contains=find)
+    msg = 'Result:' + str(data.count())
+  else:
+    msg = 'search words '
+    form = FindForm()
+    data = Friend.objects.all()
+  params = {
+    'title':'Hello',
+    'message':msg,
+    'data':data,
+  }
+  return render(request, 'hello/find.html', params)
+
+
+def index(request):
+  data = Friend.objects.all()
+  params = {
+    'title':'Hello',
+    'data':data,
+  }
+  return render(request, 'hello/index.html', params)
+
+def create(request):
+  if (request.method =='POST'):
+    obj = Friend()
+    friend = FriendForm(request.POST, instance=obj)
+    friend.save()
+    return redirect(to='/hello')
   
-  def post(self, request):
-    if ('check' in request.POST):
-      self.params['result'] = 'Checked'
-    else:
-      self.params['result'] = 'not checked'
-      
-    self.params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', self.params)
+  params = {
+    'title':'Hello',
+    'form':FriendForm(),
+  }
+  return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+  obj = Friend.objects.get(id=num)
+  if (request.method == 'POST'):
+    friend = FriendForm(request.POST, instance=obj)
+    friend.save()
+    return redirect(to='/hello')
+  params = {
+    'title':'Hello',
+    'id':num,
+    'form':FriendForm(instance=obj),
+}
+  return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+  friend = Friend.objects.get(id=num)
+  if (request.method == 'POST'):
+    friend.delete()
+    return redirect(to='/hello')
+  params = {
+    'title':'Hello',
+    'id':num,
+    'obj':friend,
+  }
+  return render(request, 'hello/delete.html', params)
+
+class FriendList(ListView):
+  model = Friend
+  
+class FriendDetail(DetailView):
+  model = Friend
