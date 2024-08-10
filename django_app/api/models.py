b00183fe3873f5770b5b672c7b4a601a8399972b
mr-ramu <raimugipan8769@gmail.com>
2024-08-10 23:18:29 +0900

    create model

---
+++ django_app/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message2(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message2_owner')
+  owner_name = models.TextField(max_length=100) #メッセージを投稿したUserの名前を保管
+  content = models.TextField(max_length=1000)
+  good_count = models.IntegerField(default=0)
+  pub_date = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return str(self.content) + '(' + str(self.owner) + ')'
+  
+  class Meta:
+    ordering = ('-pub_date',)
+    
+
+class Good2(models.Model):
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='good2_owner')
+  message = models.ForeignKey(Message2, on_delete=models.CASCADE)
+  pub_date = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return '"' + str(self.message) + '"(by ' + str(self.owner) + ')'
+  
+  class Meta:
+    ordering = ('-pub_date',)
